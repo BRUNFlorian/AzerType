867970ac53485e61b9c44249a90bd021cd0084ff
Florian BRUN <flo33.brun@hotmail.fr>
2025-01-30 12:57:00 +0100

    Code organization, adding comments

---
+++ config.js
@@ -0,0 +1,9 @@
+// Liste des mots zet des phrases
+
+const listeMots = ["Cachalot", "Pétunia", "Serviette"];
+
+const listePhrases = [
+  "Pas de panique",
+  "La vie l'univers et le reste",
+  "Merci pour les poissons",
+];
--- script.js
+++ script.js
@@ -1,42 +1,38 @@
-const listeMots = ["Cachalot", "Pétunia", "Serviette"];
-
+// Cette variable va contenir le score de l'utilisateur, il commence à zéro
 let score = 0;
 
-let premierMotÉcrit = prompt(
+// Cette variable va contenir le mot ou la phrase entré par l'utilisateur
+let motÉcrit = prompt(
   " Écrivez le mot contenu dans la première case du tableau : " + listeMots[0]
 );
 
-if (premierMotÉcrit === listeMots[0]) {
-  score++;
-  console.log("Bravo");
-  console.log(score);
-} else {
-  console.log("Vous vous êtes trompé");
-  console.log(score);
+// Tant que l'utilisateur n'a pas choisi "mots" ou "phrases", on lui repose la question
+let choix = prompt("Veuillez choisir la liste : mots ou phrases ?");
+
+while (choix != "mots" && choix != "phrases") {
+  choix = prompt("Veuillez choisir la liste : mots ou phrases ?");
 }
 
-let deuxièmeMotÉcrit = prompt(
-  " Écrivez le mot contenu dans la deuxième case du tableau : " + listeMots[1]
-);
-
-if (deuxièmeMotÉcrit === listeMots[1]) {
-  score++;
-  console.log("Bravo");
-  console.log(score);
-} else {
-  console.log("Vous vous êtes trompé");
-  console.log(score);
+// Si l'utilisateur choisi "mots", on lui demande de taper les mots de la liste
+if (choix === "mots") {
+  for (let i = 0; i < listeMots.length; i++) {
+    motÉcrit = prompt("Entrez le mot : " + listeMots[i]);
+    if (motÉcrit === listeMots[i]) {
+      score++;
+      console.log("Votre score est de : " + score + " / " + listeMots.length);
+    }
+  }
 }
 
-let troisièmeMotÉcrit = prompt(
-  " Écrivez le mot contenu dans la troisième case du tableau : " + listeMots[2]
-);
-
-if (troisièmeMotÉcrit === listeMots[2]) {
-  score++;
-  console.log("Bravo");
-  console.log(score);
-} else {
-  console.log("Vous vous êtes trompé");
-  console.log(score);
+// Si l'utilisateur choisi "phrases", on lui demande de taper les phrases de la liste
+if (choix === "phrases") {
+  for (let i = 0; i < listePhrases.length; i++) {
+    motÉcrit = prompt("Entrez la phrase : " + listePhrases[i]);
+    if (motÉcrit === listePhrases[i]) {
+      score++;
+      console.log(
+        "Votre score est de : " + score + " / " + listePhrases.length
+      );
+    }
+  }
 }
