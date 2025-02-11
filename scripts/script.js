fd414cd95972d60fd5f0996ed69d96b50d8cba7c
Florian BRUN <flo33.brun@hotmail.fr>
2025-02-11 12:45:15 +0100

    Application code improvements, openclassrooms solutions

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,61 @@
+function afficherResultat(score, nbMotsProposes) {
+    
+    let spanScore = document.querySelector(".zoneScore span")
+    
+    let affichageScore = `${score} / ${nbMotsProposes}` 
+    
+    spanScore.innerText = affichageScore
+}
+
 function afficherProposition(proposition) {
-  let zoneProposition = document.querySelector(".zoneProposition");
-  zoneProposition.innerText = proposition;
+    let zoneProposition = document.querySelector(".zoneProposition")
+    zoneProposition.innerText = proposition
 }
 
-let choixMotsOuPhrases = document.querySelectorAll(".optionSource input");
-for (let i = 0; i < choixMotsOuPhrases.length; i++) {
-  choixMotsOuPhrases[i].addEventListener("change", function () {
-    if (choixMotsOuPhrases[i].value === 1) {
-      afficherProposition(listeMots[0]);
-    } else {
-      afficherProposition(listePhrases[0]);
-    }
-  });
-}
-
-function afficherResultat(score, i) {
-  let spanScore = document.querySelector(".zoneScore span");
-  let affichageScore = `${score} / ${i}`;
-  spanScore.innerText = affichageScore;
-  console.log("Votre score est de : " + score + " sur " + i);
-}
 
 function lancerJeu() {
-  let score = 0;
-  let i = 0;
+    
+    let score = 0
+    let i = 0
+    let listeProposition = listeMots
 
-  let btnValiderMot = document.getElementById("btnValiderMot");
-  let inputEcriture = document.getElementById("inputEcriture");
-  afficherProposition(listeMots[i]);
+    let btnValiderMot = document.getElementById("btnValiderMot")
+    let inputEcriture = document.getElementById("inputEcriture")
 
-  btnValiderMot.addEventListener("click", () => {
-    console.log(inputEcriture.value);
-    if (
-      inputEcriture.value === listeMots[i] ||
-      inputEcriture.value === listePhrases[i]
-    ) {
-      score++;
+    afficherProposition(listeProposition[i])
+
+    
+    btnValiderMot.addEventListener("click", () => {
+        if (inputEcriture.value === listeProposition[i]) {
+            score++
+        }
+        i++
+        afficherResultat(score, i)
+        inputEcriture.value = ''
+        if (listeProposition[i] === undefined) {
+            afficherProposition("Le jeu est fini")
+            btnValiderMot.disabled = true
+        } else {
+            afficherProposition(listeProposition[i])
+        }
+        
+    })
+
+    
+    let listeBtnRadio = document.querySelectorAll(".optionSource input")
+    for (let index = 0; index < listeBtnRadio.length; index++) {
+        listeBtnRadio[index].addEventListener("change", (event) => {
+             
+            if (event.target.value === "1") {
+                listeProposition = listeMots
+            } else {
+                
+                listeProposition = listePhrases
+            }
+             
+            afficherProposition(listeProposition[i])
+        })
     }
-    i++;
-    afficherResultat(score, i);
-    inputEcriture.value = "";
-    if (listeMots[i] === undefined || listePhrases[i] === undefined) {
-      afficherProposition("Le jeu est maintenant terminé");
-      btnValiderMot.disabled = true;
-    } else {
-      if (choixMotsOuPhrases[0].checked) {
-        afficherProposition(listeMots[i]);
-      } else {
-        afficherProposition(listePhrases[i]);
-      }
-    }
-  });
+
+    afficherResultat(score, i)
 }
